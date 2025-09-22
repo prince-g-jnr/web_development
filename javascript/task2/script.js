d102ace2eab00be98b6f2b194875ee874a676e1f
Prince <gbolahanhardemuyiwa@gmail.com>
2025-09-22 23:57:52 +0100

    task 2

---
+++ javascript/task2/script.js
@@ -0,0 +1,24 @@
+function showArithmeticOperators() {
+    let num1 = prompt("Enter a number");
+    let num2 = prompt("Enter a number");
+
+    let addition = num1 + num2;
+    let subtraction = num1 - num2;
+    let multiplication = num1 * num2;
+    let division = num1 / num2;
+    let remainder = num1 % num2;
+    let exponentiation = num1 ** num2;
+
+    let output = `
+        <strong>Arithmetic Operators:</strong><br><br>
+        num1 = ${num1}, num2 =${num2}<br><br>
+        Addition (num1 +  num2): ${addition}<br>
+        Subtraction (num1 - num2): ${subtraction}<br>
+        Multiplication (num1 * num2): ${multiplication}<br>
+        Division (num1 / num2): ${division}<br>
+        Remainder (num1 % num2): ${remainder}<br>
+        Exponentiation (num1 ** num2): ${exponentiation}
+    `;
+
+    document.getElementById('operators-output').innerHTML = output;
+}
